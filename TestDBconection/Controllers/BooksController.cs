61b2ac311b98d82ada8c28734efc3d0b31eaf149
taichi88 <nikoloz.gabeskiria@gmail.com>
2025-01-15 16:35:06 +0400

    Add project files.

---
+++ TestDBconection/Controllers/BooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestDBconection.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestDBconection/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestDBconection.Models;
+
+
+namespace TestDBconection.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        {
+
+        }
+
+        public DbSet<BooksModel> BooksModelTable { get; set; }
+
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BooksModel>().HasData(
+                new BooksModel { Id = 1, date = DateTime.Now, Description = "sdad", Name = "sdawqe" }
+
+                );
+        }*/
+
+    }
+}
